08cc46a6e4306a19d9bd132005e395023e9b94d1
KylerReed <113257958+KylerReed@users.noreply.github.com>
2026-03-06 18:50:28 -0500

    Used for Annual Volunteer reports

--- annual_report.py
+++ annual_report.py
@@ -1,1 +1,52 @@
+from datetime import datetime
+from volunteerTracker import Volunteer
+
+class AnnualReport:
+  """ Generates an annual recap of volunteer activity """
+
+  @staticmethod
+  def generate(year: int):
+    volunteers = Volunteer.getAllVolunteers()
+
+    report = {
+      "year": year,
+      "total_hours": 0,
+      "volunteer_count": 0,
+      "details": []
+    }
+
+  for volunteer in volunteers:
+    yearly_hours = 0
+
+    for record in volunteer.hours:
+      date = datetime.strptime(record["date"], "%Y-%m-%d")
+
+      if date.year == year:
+        yearly_hours += record["hours"]
+
+    if yearly_hours > 0:
+      report["volunteer_count"] += 1
+      report["total_hours"] += yearly_hours
+
+      report["details"].append({
+        "name": volunteer.name,
+        "hours": yearly.hours
+      })
+  return report
+
+if __name__ == "__main__":
+  year = int(input("Enter year: "))
+
+  report = AnnualReport.generate(year)
+
+  print("\nAnnual Volunteer Report")
+  print("----------------------")
+  print(f"Year: {report["year"]}")
+  print(f"Total Volunteers: {report["volunteer_count"]}")
+  print(f"Total Hours: {report["total_hours"]}\n")
+
+  for v in report["details"]:
+    print(f"{v["name"]} - {v["hours"]} hours")
+
+
 
